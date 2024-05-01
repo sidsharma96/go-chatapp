1095df376e713b2378a99d0bfcf8c363db3c697c
sidsharma96 <siddharthsharma@Siddharths-MacBook-Pro.local>
2024-05-01 13:21:31 -0700

    Fixing catch all route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,7 +25,6 @@ const App: Component = () => {
           <Route path='/profile' component={Profile}></Route>
           <Route path='/chat/:name' component={Chat}></Route>
           <Route path='/' component={Home}></Route>
-          <Route path={'*'} component={Login} />
         </Router>
       </WebSocketContextProvider>
     </AuthContextProvider>
